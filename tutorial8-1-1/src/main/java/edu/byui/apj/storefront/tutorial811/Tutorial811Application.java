c3aa79afdece110ac33ea93fd1a890cc5eb412bd
Isai Mar <mar18026@byui.edu>
2025-04-02 15:24:00 -0600

    New tutorial8-1

---
+++ tutorial8-1-1/src/main/java/edu/byui/apj/storefront/tutorial811/Tutorial811Application.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial811;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial811Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tutorial811Application.class, args);
+    }
+
+}
