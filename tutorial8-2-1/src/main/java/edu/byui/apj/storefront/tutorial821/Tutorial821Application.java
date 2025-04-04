804fdd4ada32da65ea91cd688d4b0dec05dae335
Isai Mar <mar18026@byui.edu>
2025-04-03 20:54:33 -0600

    commit

---
+++ tutorial8-2-1/src/main/java/edu/byui/apj/storefront/tutorial821/Tutorial821Application.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial821;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial821Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial821Application.class, args);
+	}
+
+}
---
+++ tutorial8-2-1/src/test/java/edu/byui/apj/storefront/tutorial821/Tutorial821ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.byui.apj.storefront.tutorial821;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tutorial821ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
