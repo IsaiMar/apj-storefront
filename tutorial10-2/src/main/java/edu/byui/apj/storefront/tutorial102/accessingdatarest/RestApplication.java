c33d5b1a145c6faf720b70fc3c44a8435b7446d7
Isai Mar <mar18026@byui.edu>
2025-04-05 20:56:19 -0600

    mongodb and rest tutorial10-2

---
+++ tutorial10-2/src/main/java/edu/byui/apj/storefront/tutorial102/accessingdatarest/RestApplication.java
@@ -0,0 +1,12 @@
+package edu.byui.apj.storefront.tutorial102.accessingdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+}
